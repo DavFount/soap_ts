61b3088a13ea7b600742b182ee739f8e925bfa05
DavFount <47730893+DavFount@users.noreply.github.com>
2022-11-27 23:32:15 -0800

    Added Try/Catch blocks to routes

--- src/routes/friend.routes.ts
+++ src/routes/friend.routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import { getErrorMessage } from '../utils/errors.util';
 import auth from '../middleware/auth';
 const router = Router();
 
@@ -6,33 +7,43 @@ const router = Router();
 import Profile, { IProfile } from '../models/profile.model';
 
 router.route('/add').put(auth, async (req: Request, res: Response) => {
-  const { userId, friendId } = req.body;
+  try {
+    const { userId, friendId } = req.body;
 
-  await Profile.updateOne(
-    {
-      userId
-    },
-    {
-      $push: { friends: friendId }
-    }
-  );
+    await Profile.updateOne(
+      {
+        userId
+      },
+      {
+        $push: { friends: friendId }
+      }
+    );
 
-  res.status(200).json({ msg: 'Friend Added Successfully' });
+    res.status(200).json({ msg: 'Friend Added Successfully' });
+  } catch (error) {
+    console.error(getErrorMessage(error));
+    res.status(500).send('Server Error');
+  }
 });
 
 router.route('/remove').put(auth, async (req: Request, res: Response) => {
-  const { userId, friendId } = req.body;
+  try {
+    const { userId, friendId } = req.body;
 
-  await Profile.updateOne(
-    {
-      userId
-    },
-    {
-      $pull: { friends: friendId }
-    }
-  );
+    await Profile.updateOne(
+      {
+        userId
+      },
+      {
+        $pull: { friends: friendId }
+      }
+    );
 
-  res.status(200).json({ msg: 'Friend Removed Successfully' });
+    res.status(200).json({ msg: 'Friend Removed Successfully' });
+  } catch (error) {
+    console.error(getErrorMessage(error));
+    res.status(500).send('Server Error');
+  }
 });
 
 export default router;
--- src/routes/language.routes.ts
+++ src/routes/language.routes.ts
@@ -1,12 +1,18 @@
 import { Router, Request, Response } from 'express';
+import { getErrorMessage } from '../utils/errors.util';
 import auth from '../middleware/auth';
 const router = Router();
 
 import Language, { ILanguage } from '../models/language.model';
 
 router.route('/').get(auth, async (req: Request, res: Response) => {
-  const languages = await Language.find();
-  return res.json(languages);
+  try {
+    const languages = await Language.find();
+    return res.json(languages);
+  } catch (error) {
+    console.error(getErrorMessage(error));
+    res.status(500).send('Server Error');
+  }
 });
 
 export default router;
